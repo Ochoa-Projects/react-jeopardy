988737bb4f3d5f58a1899c7b490e94b9316e415b
vincentochoa3 <vochoa893@gmail.com>
2023-01-23 13:16:14 -0800

    add basic homepage outline and css styles module

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from "./styles.module.css";
 
 export default function Home() {
   return (
@@ -6,7 +7,18 @@ export default function Home() {
       <Head>
         <title>React Jeopardy</title>
       </Head>
-      <h1>React.js Jeopardy!</h1>
+      <div>
+        <h1>React.js Jeopardy!</h1>
+        <h3>Hone in on your React skills with React Jeopardy!</h3>
+        <h2>Game Difficulty</h2>
+        <div>
+          <p>Easy</p>
+          <p>Normal</p>
+          <p>Hard</p>
+        </div>
+        <input type="text" placeholder="Enter Your Name" />
+        <button>Start</button>
+      </div>
     </>
   );
 }
