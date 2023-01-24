8ec3bc589221ba1c3f80599dfbfbe3bf26e40fbb
vincentochoa3 <vochoa893@gmail.com>
2023-01-24 15:32:47 -0800

    add state for difficulty selection, form for user's information, and css refinement

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import Head from "next/head";
 import PageContainer from "../components/PageContainer";
 import styles from "./styles.module.css";
 
 export default function Home() {
+  const [selected, setSelected] = useState("Normal");
+  const difficulties = ["Easy", "Normal", "Hard"];
+
+  const handleSelected = (difficulty) => {
+    selected !== difficulty ? setSelected(difficulty) : null;
+  };
+
   return (
     <>
       <Head>
@@ -12,17 +20,29 @@ export default function Home() {
         <h1 className={styles.homepageTitle}>React.js Jeopardy!</h1>
         <h3>Hone in on your React skills with React Jeopardy!</h3>
         <h2>Game Difficulty</h2>
-        <div className={styles.difficultySelection}>
-          <div>Easy</div>
-          <div>Normal</div>
-          <div>Hard</div>
-        </div>
-        <input
-          type="text"
-          placeholder="Enter Your Name"
-          className={styles.nameInput}
-        />
-        <button>Start</button>
+        <form className={styles.homepageForm}>
+          <div className={styles.difficultySelection}>
+            {difficulties.map((difficulty) => (
+              <div
+                key={difficulty}
+                className={`${selected === difficulty && styles.selected} ${
+                  styles.difficultyButton
+                }`}
+                onClick={() => handleSelected(difficulty)}
+              >
+                {difficulty}
+              </div>
+            ))}
+          </div>
+          <input
+            type="text"
+            placeholder="Enter Your Name"
+            className={styles.nameInput}
+          />
+          <button type="submit" className={styles.startButton}>
+            START
+          </button>
+        </form>
       </PageContainer>
     </>
   );
