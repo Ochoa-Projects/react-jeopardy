535267d446b0a88f2503177324c0e1f8b7c1213a
vincentochoa3 <vochoa893@gmail.com>
2023-01-24 18:58:20 -0800

    outline for top row and category row

--- pages/gameboard/index.js
+++ pages/gameboard/index.js
@@ -2,12 +2,26 @@ import PageContainer from "../../components/PageContainer";
 import styles from "./styles.module.css";
 
 export default function Gameboard() {
-  const mockCategories = [];
+  const mockCategories = [
+    "Category1",
+    "Category2",
+    "Category3",
+    "Category4",
+    "Category5",
+  ];
 
   return (
     <PageContainer>
-      <div className={styles.topRow}>Top Row</div>
-      <div className={styles.categoryRow}>Categories</div>
+      <div className={styles.topRow}>
+        <h1>Single Jeopardy</h1>
+        <div className={styles.playerScores}></div>
+        <div className={styles.menuButton}>&#9776;</div>
+      </div>
+      <div className={styles.categoryRow}>
+        {mockCategories.map((category) => (
+          <div className={styles.categoryContainer}>{category}</div>
+        ))}
+      </div>
       <div className={styles.valueGrid}>Value Grid</div>
     </PageContainer>
   );
