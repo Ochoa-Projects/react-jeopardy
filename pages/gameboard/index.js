c2f7afb052e7db07dea66142f5b4bf93079838c4
vincentochoa3 <vochoa893@gmail.com>
2023-01-24 17:04:10 -0800

    add rows for page and grid for question value

--- pages/gameboard/index.js
+++ pages/gameboard/index.js
@@ -1,5 +1,14 @@
 import PageContainer from "../../components/PageContainer";
+import styles from "./styles.module.css";
 
 export default function Gameboard() {
-  return <PageContainer>Gameboard Page</PageContainer>;
+  const mockCategories = [];
+
+  return (
+    <PageContainer>
+      <div className={styles.topRow}>Top Row</div>
+      <div className={styles.categoryRow}>Categories</div>
+      <div className={styles.valueGrid}>Value Grid</div>
+    </PageContainer>
+  );
 }
