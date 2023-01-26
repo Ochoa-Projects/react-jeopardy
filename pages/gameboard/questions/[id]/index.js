b1e7ebcd119896135d66f3147ed30fa2609c499f
vincentochoa3 <vochoa893@gmail.com>
2023-01-26 12:39:38 -0800

    add jsx structure

--- pages/gameboard/questions/[id]/index.js
+++ pages/gameboard/questions/[id]/index.js
@@ -1,5 +1,21 @@
 import PageContainer from "../../../../components/PageContainer";
 
 export default function Question() {
-  return <PageContainer>Question</PageContainer>;
+  return (
+    <PageContainer>
+      <h1>CATEGORY - $600</h1>
+      <div>
+        <h2>
+          This React tool is used to share state and other important information
+          with the use of "prop drilling".
+        </h2>
+      </div>
+      <p>X seconds remaining...</p>
+      <form method="post">
+        <label for="answer">Please Enter your answer:</label>
+        <input id="answer" placeholder="Answer here..." />
+        <button type="submit">Submit</button>
+      </form>
+    </PageContainer>
+  );
 }
