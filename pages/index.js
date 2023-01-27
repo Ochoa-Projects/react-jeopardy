6b01d9a9ad60c9d430c2e3d877f874ce6f4e1e2f
vincentochoa3 <vochoa893@gmail.com>
2023-01-26 17:50:20 -0800

    add css for formatting and using color variables

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
       <PageContainer>
         <h1 className={styles.homepageTitle}>React.js Jeopardy!</h1>
         <h3>Hone in on your React skills with React Jeopardy!</h3>
-        <h2>Game Difficulty</h2>
+        <h2 className={styles.difficultyHeading}>Game Difficulty</h2>
         <form action="/gameboard" className={styles.homepageForm}>
           <div className={styles.difficultySelection}>
             {difficulties.map((difficulty) => (
