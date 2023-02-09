a06da46e264f53e7ff3ad068bf290c1386c5c279
vincentochoa3 <vochoa893@gmail.com>
2023-02-08 16:06:41 -0800

    add different animations for different pages

--- components/PageContainer/index.js
+++ components/PageContainer/index.js
@@ -1,17 +1,10 @@
 import styles from "./styles.module.css";
-import { motion as m } from "framer-motion";
 
 const PageContainer = ({ children, color = "--dark-blue" }) => {
   return (
-    <m.div
-      initial={{ opacity: 0, scale: 0.1 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ ease: "easeOut", duration: 0.5 }}
-      style={{ background: color }}
-      className={styles.pageContainer}
-    >
+    <div style={{ background: color }} className={styles.pageContainer}>
       {children}
-    </m.div>
+    </div>
   );
 };
 
--- pages/gameboard/questions/[id]/index.js
+++ pages/gameboard/questions/[id]/index.js
@@ -1,21 +1,29 @@
+import { motion as m } from "framer-motion";
 import PageContainer from "../../../../components/PageContainer";
 import styles from "./styles.module.css";
 
 export default function Question() {
   return (
-    <PageContainer color="var(--light-blue-gradient)">
-      <h1 className={styles.questionHeading}>CATEGORY4 - $600</h1>
-      <div className={styles.questionContainer}>
-        <h2>
-          This React tool is used to share state and other important information
-          without the use of "prop drilling".
-        </h2>
-      </div>
-      <p className={styles.timer}>X seconds remaining...</p>
-      <form method="post" action="/gameboard" className={styles.answerForm}>
-        <input id="answer" placeholder="Enter answer here..." />
-        <button type="submit">Submit</button>
-      </form>
+    <PageContainer>
+      <m.div
+        className={styles.container}
+        initial={{ rotateY: 180, scale: 0.2 }}
+        animate={{ rotateY: 0, scale: 1 }}
+        transition={{ duration: 0.7 }}
+      >
+        <h1 className={styles.questionHeading}>CATEGORY4 - $600</h1>
+        <div className={styles.questionContainer}>
+          <h2>
+            This React tool is used to share state and other important
+            information without the use of "prop drilling".
+          </h2>
+        </div>
+        <p className={styles.timer}>X seconds remaining...</p>
+        <form method="post" action="/gameboard" className={styles.answerForm}>
+          <input id="answer" placeholder="Enter answer here..." />
+          <button type="submit">Submit</button>
+        </form>
+      </m.div>
     </PageContainer>
   );
 }
