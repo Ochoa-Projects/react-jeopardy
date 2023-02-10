c7798de9a07201e085335b48664346e3338c03e1
gochoa1088 <gochoa1088@gmail.com>
2023-02-09 16:09:43 -0800

    remove api directory, using getServerSideProps

--- pages/api/response.js
+++
@@ -1,7 +0,0 @@
-import { getKnex } from "../../knex/knexcache.js";
-
-export default async function handler(req, res) {
-  const knex = getKnex();
-  const response = await knex("questions");
-  res.status(200).json(response);
-}
