5f76b10630cd95bb43359e779e84123ad30997ae
vincentochoa3 <vochoa893@gmail.com>
2023-02-10 17:28:22 -0800

    add use of localStorage to persist clicked ValueCards

--- components/ValueBoard/index.js
+++ components/ValueBoard/index.js
@@ -1,7 +1,16 @@
-import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
 import ValueCard from "../ValueCard";
+import styles from "./styles.module.css";
 
 const ValueBoard = () => {
+  const [attempted, setAttempted] = useState(() => {
+    if (typeof window !== "undefined") {
+      const saved = window.localStorage.getItem("attempted");
+      const initialValue = JSON.parse(saved);
+      return initialValue || { valueCards: [] };
+    }
+  });
+
   const mockQuestions = {
     cat1: ["col1row1", "col1row2", "col1row3", "col1row4", "col1row5"],
     cat2: ["col2row1", "col2row2", "col2row3", "col2row4", "col2row5"],
@@ -9,12 +18,19 @@ const ValueBoard = () => {
     cat4: ["col4row1", "col4row2", "col4row3", "col4row4", "col4row5"],
     cat5: ["col5row1", "col5row2", "col5row3", "col5row4", "col5row5"],
   };
+  useEffect(() => {
+    window.localStorage.setItem("attempted", JSON.stringify(attempted));
+  }, [attempted]);
+
+  useEffect(() => {
+    setAttempted(JSON.parse(window.localStorage.getItem("attempted")));
+  }, []);
 
   return (
     <div className={styles.valueGrid}>
       {Object.keys(mockQuestions).map((key, i) =>
         mockQuestions[key].map((question, j) => (
-          <ValueCard i={i} j={j} question={question} />
+          <ValueCard i={i} j={j} key={question} setAttempted={setAttempted} />
         ))
       )}
     </div>
--- components/ValueCard/index.js
+++ components/ValueCard/index.js
@@ -1,14 +1,19 @@
-import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "./styles.module.css";
 
-const ValueCard = ({ i, j, question }) => {
+const ValueCard = ({ i, j, setAttempted }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    setAttempted((prev) => ({ valueCards: [...prev.valueCards, `${i}${j}`] }));
+    router.push("/gameboard/questions/1");
+  };
+
   return (
     <div
       className={styles.valueBoardItem}
       style={{ gridColumn: i + 1, gridRow: j + 1 }}
-      key={question}
     >
-      <Link href="/gameboard/questions/1">${(j + 1) * 200}</Link>
+      <div onClick={handleClick}>${(j + 1) * 200}</div>
     </div>
   );
 };
