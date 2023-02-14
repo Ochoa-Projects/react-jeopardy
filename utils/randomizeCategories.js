22fd91177384081b432281e40636a7602c0fde01
gochoa1088 <gochoa1088@gmail.com>
2023-02-14 01:16:58 -0800

    refactor slug and category queries

--- utils/randomizeCategories.js
+++ utils/randomizeCategories.js
@@ -2,9 +2,11 @@ import { getKnex } from "../knex/knexcache";
 
 const randomizeCategories = async () => {
   const knex = getKnex();
-  const categoriesResponse = await knex("questions").distinct("category");
-  const categories = categoriesResponse.map((item) => item.category);
-  const shuffledCategories = categories.sort(() => 0.5 - Math.random());
+
+  const categoriesResponse = await knex("questions")
+    .distinct("category")
+    .pluck("category");
+  const shuffledCategories = categoriesResponse.sort(() => 0.5 - Math.random());
   const singleCategories = shuffledCategories.slice(0, 5);
   const doubleCategories = shuffledCategories.slice(5, 12);
 
--- utils/randomizeSlugs.js
+++ utils/randomizeSlugs.js
@@ -7,9 +7,9 @@ const randomizeSlugs = async (categories) => {
   for (let i = 0; i < categories.length; i++) {
     const slugResponse = await knex("questions")
       .where("category", categories[i])
-      .select("question_slug");
-    const categorySlugs = slugResponse.map((slug) => slug.question_slug);
-    const shuffledSlugs = categorySlugs.sort(() => 0.5 - Math.random());
+      .select("question_slug")
+      .pluck("question_slug");
+    const shuffledSlugs = slugResponse.sort(() => 0.5 - Math.random());
     slugs[categories[i]] = shuffledSlugs.slice(0, 5);
   }
 
