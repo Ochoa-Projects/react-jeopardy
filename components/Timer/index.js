51fe29d8545611c8673a1a8bcba2e8c88b937beb
vincentochoa3 <vochoa893@gmail.com>
2023-02-13 21:49:59 -0800

    add logic to render timer countdown

--- components/Timer/index.js
+++ components/Timer/index.js
@@ -3,7 +3,24 @@ import { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 
 const Timer = ({ seconds }) => {
-  return <div className={styles.timer}>{`${seconds} seconds remaining`}</div>;
+  const [secondsRemaining, setSecondsRemaining] = useState(seconds);
+
+  const decrementTimer = () => setSecondsRemaining((prev) => prev - 1);
+
+  useEffect(() => {
+    if (secondsRemaining <= 0) return;
+
+    const interval = setInterval(decrementTimer, 1000);
+
+    return () => clearInterval(interval);
+  }, [decrementTimer]);
+
+  return (
+    <div className={styles.timer}>
+      <span>{secondsRemaining}</span>
+      <p>seconds remaining...</p>
+    </div>
+  );
 };
 
 export default Timer;
