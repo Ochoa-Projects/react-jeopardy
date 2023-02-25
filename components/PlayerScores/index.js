69684b9269f064eaf273850417f76c784c732f2d
Gino Ochoa <gochoa1088@gmail.com>
2023-02-24 21:34:29 -0800

    appropriately set player name state

--- components/PlayerScores/index.js
+++ components/PlayerScores/index.js
@@ -1,12 +1,23 @@
+import { useGame } from "../../context/GameContext";
 import styles from "./styles.module.css";
 
 const PlayerScores = () => {
+  const {
+    playerScores: { player1, player2, player3 },
+  } = useGame();
+  console.log(player1);
   return (
     <div className={styles.playerScoresBorder}>
       <div className={styles.playerScores}>
-        <div>Player1: $$$</div>
-        <div>Player2: $$$</div>
-        <div>Player3: $$$</div>
+        <div className={styles.playerContainer}>
+          {player1.name}:<span>${player1.score}</span>
+        </div>
+        <div className={styles.playerContainer}>
+          {player2.name}: <span>${player2.score}</span>
+        </div>
+        <div className={styles.playerContainer}>
+          {player3.name}: <span>${player3.score}</span>
+        </div>
       </div>
     </div>
   );
--- context/GameContext.js
+++ context/GameContext.js
@@ -3,6 +3,20 @@ import { createContext, useContext, useState } from "react";
 const GameContext = createContext();
 
 export const useGame = () => useContext(GameContext);
+export const initialPlayerState = {
+  player1: {
+    name: "",
+    score: 0,
+  },
+  player2: {
+    name: "Jerry Javascript",
+    score: 0,
+  },
+  player3: {
+    name: "Robin React",
+    score: 0,
+  },
+};
 
 export const GameContextProvider = ({ children }) => {
   const [singleCategories, setSingleCategories] = useState([]);
@@ -11,20 +25,7 @@ export const GameContextProvider = ({ children }) => {
   const [doubleSlugs, setDoubleSlugs] = useState([]);
   const [attempts, setAttempts] = useState([]);
   const [selectedDifficulty, setSelectedDifficulty] = useState("Normal");
-  const [playerScores, setPlayerScores] = useState({
-    0: {
-      name: "",
-      score: 0,
-    },
-    1: {
-      name: "Jerry Javascript",
-      score: 0,
-    },
-    2: {
-      name: "Robin React",
-      score: 0,
-    },
-  });
+  const [playerScores, setPlayerScores] = useState(initialPlayerState);
 
   return (
     <GameContext.Provider
