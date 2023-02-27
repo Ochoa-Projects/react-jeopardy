4fc8a22c2ae2bf2d0cb14f79cd8da17c5cfd426a
vincentochoa3 <vochoa893@gmail.com>
2023-02-27 13:15:33 -0800

    refactor gameboard routes for single and double stages

--- components/ValueCard/index.js
+++ components/ValueCard/index.js
@@ -1,16 +1,29 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useGame } from "../../context/GameContext";
 import styles from "./styles.module.css";
 
 const ValueCard = ({ i, j, slug }) => {
+  const [gameStage, setGameStage] = useState();
   const router = useRouter();
   const { attempts, setAttempts } = useGame();
 
   const value = (j + 1) * 200;
 
+  useEffect(() => {
+    if (attempts.length < 25) {
+      setGameStage("single");
+    } else {
+      setGameStage("double");
+    }
+  }, []);
+
   const handleValueCardClick = () => {
     setAttempts((prev) => [...prev, `${i}${j}`]);
-    router.push({ pathname: `/gameboard/questions/${slug}`, query: { value } });
+    router.push({
+      pathname: `/gameboard/${gameStage}/questions/${slug}`,
+      query: { value },
+    });
   };
 
   const isAttempted = attempts.includes(`${i}${j}`);
