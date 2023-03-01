10c8c0afdc3c4c62f55e933f32272a8a8413dd8e
vincentochoa3 <vochoa893@gmail.com>
2023-03-01 14:52:44 -0800

    add CorrectAnswer component

---
+++ components/CorrectAnswer/index.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+const CorrectAnswer = () => {
+  return (
+    <div>
+      <span>Correct Answer: {answer}</span>
+    </div>
+  );
+};
+
+export default CorrectAnswer;
