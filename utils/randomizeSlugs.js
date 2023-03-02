ef1d6fd1707362fe67d2ac6366f6058c20d80ad7
Gino Ochoa <gochoa1088@gmail.com>
2023-03-02 01:10:21 -0800

    randomize dailyDouble

--- utils/randomizeSlugs.js
+++ utils/randomizeSlugs.js
@@ -2,17 +2,25 @@ import { getKnex } from "../knex/knexcache";
 
 const randomizeSlugs = async (categories) => {
   const knex = getKnex();
-  const slugs = {};
+  const randomCategory = Math.ceil(Math.random() * 5) - 1;
+  const randomQuestion = Math.ceil(Math.random() * 5) - 1;
+  console.log(randomCategory, randomQuestion);
 
+  const slugs = {};
   for (let i = 0; i < categories.length; i++) {
     const slugResponse = await knex("questions")
       .where("category", categories[i])
       .select("question_slug")
       .pluck("question_slug");
     const shuffledSlugs = slugResponse.sort(() => 0.5 - Math.random());
-    slugs[categories[i]] = shuffledSlugs.slice(0, 5);
+    const dailyDoubledSlugs = shuffledSlugs.map((slug, index) => ({
+      slug,
+      isDailyDouble:
+        randomCategory === i && randomQuestion === index ? true : false,
+    }));
+    slugs[categories[i]] = dailyDoubledSlugs.slice(0, 5);
   }
-
+  console.log(slugs);
   return slugs;
 };
 
