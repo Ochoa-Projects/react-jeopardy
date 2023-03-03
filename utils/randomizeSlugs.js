6d646f5f59faccabb70559c25e000d8d13faf3f0
Gino Ochoa <gochoa1088@gmail.com>
2023-03-02 19:33:30 -0800

    fix randomizeSlug bug

--- utils/randomizeSlugs.js
+++ utils/randomizeSlugs.js
@@ -4,6 +4,7 @@ const randomizeSlugs = async (categories) => {
   const knex = getKnex();
   const randomCategory = Math.ceil(Math.random() * 5) - 1;
   const randomQuestion = Math.ceil(Math.random() * 5) - 1;
+  console.log(randomCategory, randomQuestion);
 
   const slugs = {};
   for (let i = 0; i < categories.length; i++) {
@@ -11,13 +12,14 @@ const randomizeSlugs = async (categories) => {
       .where("category", categories[i])
       .select("question_slug")
       .pluck("question_slug");
-    const shuffledSlugs = slugResponse.sort(() => 0.5 - Math.random());
-    const dailyDoubledSlugs = shuffledSlugs.map((slug, index) => ({
+    const shuffledSlugs = slugResponse
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 5);
+    slugs[categories[i]] = shuffledSlugs.map((slug, index) => ({
       slug,
       isDailyDouble:
         randomCategory === i && randomQuestion === index ? true : false,
     }));
-    slugs[categories[i]] = dailyDoubledSlugs.slice(0, 5);
   }
   return slugs;
 };
