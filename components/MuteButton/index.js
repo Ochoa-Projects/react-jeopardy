68f5af094a6151ecb659ff58e5c0de0c5ad17c46
vincentochoa3 <vochoa893@gmail.com>
2023-03-08 11:24:57 -0800

    add volume svgs and render volume-on.svg on homepage

---
+++ components/MuteButton/index.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+const MuteButton = () => {
+  return (
+    <div className={styles.menuButtonContainer}>
+      <div className={styles.muteOff} />
+    </div>
+  );
+};
+
+export default MuteButton;
