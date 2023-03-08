af4a10c4849f8e2d0121daa4a5f45086b13e3763
vincentochoa3 <vochoa893@gmail.com>
2023-03-08 15:32:54 -0800

    add small-win.wav to audio context

--- context/AudioContext.js
+++ context/AudioContext.js
@@ -15,22 +15,34 @@ export const AudioContextProvider = ({ children }) => {
   const [timesUpAudio] = useState(
     typeof Audio !== "undefined" && new Audio("/sounds/times-up.mp3")
   );
+  const [smallWinAudio] = useState(
+    typeof Audio !== "undefined" && new Audio("/sounds/small-win.mp3")
+  );
 
   useEffect(() => {
     if (isMuted) {
       introAudio.volume = 0;
       thinkingAudio.volume = 0;
       timesUpAudio.volume = 0;
+      smallWinAudio.volume = 0;
     } else {
       introAudio.volume = 0.1;
       thinkingAudio.volume = 0.1;
       timesUpAudio.volume = 0.1;
+      smallWinAudio.volume = 0.1;
     }
   }, [isMuted]);
 
   return (
     <AudioContext.Provider
-      value={{ isMuted, setIsMuted, introAudio, thinkingAudio, timesUpAudio }}
+      value={{
+        isMuted,
+        setIsMuted,
+        introAudio,
+        thinkingAudio,
+        timesUpAudio,
+        smallWinAudio,
+      }}
     >
       {children}
     </AudioContext.Provider>
