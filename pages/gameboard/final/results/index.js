3485e0e01a030cf45ca13f721c1a38601704eaaa
vincentochoa3 <vochoa893@gmail.com>
2023-03-13 15:11:43 -0700

    add endAudio to final results page

--- pages/gameboard/final/results/index.js
+++ pages/gameboard/final/results/index.js
@@ -1,13 +1,26 @@
+import { useEffect } from "react";
 import PageContainer from "../../../../components/PageContainer";
 import Results from "../../../../components/Results";
 import ResultsButton from "../../../../components/ResultsButton";
 import ResultsHeading from "../../../../components/ResultsHeading";
 import { useGame } from "../../../../context/GameContext";
+import { useAudio } from "../../../../context/AudioContext";
 
 const FinalResults = () => {
   const { playerScores } = useGame();
+  const { endAudio } = useAudio();
   const playerValues = Object.values(playerScores);
   const sortedPlayerValues = playerValues.sort((a, b) => b.score - a.score);
+
+  useEffect(() => {
+    setTimeout(() => {
+      endAudio.currentTime = 0.8;
+      endAudio.play();
+    });
+    return () => {
+      endAudio.pause();
+    };
+  }, []);
 
   return (
     <PageContainer>
