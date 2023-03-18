cbe323f0f469fa09288bb0b50ab975ad76fde888
Gino Ochoa <gochoa1088@gmail.com>
2023-03-17 17:04:11 -0700

    knexfile pg dev setup

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
 const { loadEnvConfig } = require("@next/env");
-
-const dev = process.env.NODE_ENV !== "production";
-const { DB_ENVIRONMENT } = loadEnvConfig("./", dev).combinedEnv;
+loadEnvConfig("./");
 
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/questions.sqlite3",
+    client: "pg",
+    connection: process.env.DB_URL,
+    pool: {
+      min: 2,
+      max: 10,
     },
     migrations: {
       directory: "./knex/migrations",
@@ -15,10 +15,19 @@ module.exports = {
     seeds: {
       directory: "./knex/seeds",
     },
-    useNullAsDefault: true,
   },
   production: {
     client: "pg",
-    connection: DB_ENVIRONMENT,
+    connection: process.env.DB_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./knex/migrations",
+    },
+    seeds: {
+      directory: "./knex/seeds",
+    },
   },
 };
