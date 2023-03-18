32f292aa1f11c0ffbcd7cb995b68fc4c478dd9b9
Gino Ochoa <gochoa1088@gmail.com>
2023-03-17 17:05:18 -0700

    getFinalSlug update/fix

--- utils/getFinalSlug.js
+++ utils/getFinalSlug.js
@@ -3,7 +3,7 @@ import { getKnex } from "../knex/knexcache";
 const getFinalSlug = async (category) => {
   const knex = getKnex();
   const slugResponse = await knex("questions")
-    .where("category", category)
+    .where("category", category[0])
     .select("question_slug")
     .pluck("question_slug");
   const shuffledSlugs = slugResponse.sort(() => 0.5 - Math.random());
