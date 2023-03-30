8d32111097f73a2e377335a2a9152c89798e2d27
vincentochoa3 <vochoa893@gmail.com>
2023-03-29 21:09:17 -0700

    take out unecessary useState

--- components/PushAnimation/index.js
+++ components/PushAnimation/index.js
@@ -1,28 +1,18 @@
 import { motion as m } from "framer-motion";
-import { useState, useEffect } from "react";
 import Loading from "../Loading";
 import styles from "./styles.module.css";
 
 const PushAnimation = ({ children, isGameboard, width }) => {
-  const [windowWidth, setWindowWidth] = useState();
-
-  useEffect(() => {
-    setWindowWidth(width);
-  }, [windowWidth]);
-
   if (!width) {
-    console.log("inside");
     return <Loading />;
   }
-
-  console.log("outside");
 
   return (
     <m.div
       initial={{ x: "-100vw" }}
       animate={{ x: 0 }}
       transition={
-        windowWidth > 414
+        width > 414
           ? { delay: 0.4, ease: "backOut", duration: 0.6 }
           : { delay: 0.4, duration: 0.6 }
       }
